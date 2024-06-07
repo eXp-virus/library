55d1c00d08f7816c70c73a96ae38f9161a44cd07
Rupesh <56559923+eXp-virus@users.noreply.github.com>
2024-06-07 17:46:02 +0530

    Display user input from a form in a table

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-const library = [{title: 'Sample', pages: 100, author: 'sample', read: true},
-                {title: 'Sample2', pages: 1001, author: 'sample2', read: false}];
+const library = [{title: 'Sample', pages: 100, author: 'sample', read: true}];
+
+function Book(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    const book = new Book(bookName.value, authorName.value, pages.value, haveRead.checked);
+    library.push(book);
+}
+
 
 const tdata = document.querySelector('#add');
-const btn = document.querySelector('button');
+const modal = document.querySelector('#modal-button');
+const dialog = document.querySelector('dialog');
+const btn = document.querySelector('#submit');
 
-btn.addEventListener('click', () => {
+let bookName = document.querySelector('#name');
+let authorName = document.querySelector('#author');
+let bookPages = document.querySelector('#pages');
+let haveRead = document.querySelector('#haveRead');
+
+modal.addEventListener('click', () => {
+    dialog.showModal();
+})
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    dialog.close(); // close modal
     displayData();
 })
 
 function displayData() {
     // declared empty array to store all the values in each objects in {library} array
     let data = []
-    // loop through all the key:values in each object and push values to data[]
-    for(let i=0; i < library.length; i++) {
+    // loop through the key:values in the last object and push values to data[]
+    for(let i = library.length; i >= library.length - 1; i--) {
         for(let j in library[i]) {
             data.push(library[i][j]);
         }
