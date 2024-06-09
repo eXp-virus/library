d122fc350a76f2db7b0eec8b88d43540eb29f9d2
Rupesh <56559923+eXp-virus@users.noreply.github.com>
2024-06-09 16:52:44 +0530

    Add functionality to remove books from table

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const library = [{title: 'Sample', pages: 100, author: 'sample', read: true}];
+const library = [];
 
 function Book(name, author, pages, read) {
     this.name = name;
@@ -12,16 +12,31 @@ function addBookToLibrary() {
     library.push(book);
 }
 
+let button;
+function createButton() { // create a button to remove a entry from display inside table
+    button = document.createElement('button');
+    button.textContent = 'X';
+    button.classList.toggle('delete-btn');
+    return button;
+}
+
+function createCheckbox() {
+    const box = document.createElement('input');
+    box.setAttribute('type', 'checkbox');
+    if(haveRead.checked) box.checked = true;
+    return box;
+}
 
 const tdata = document.querySelector('#add');
 const modal = document.querySelector('#modal-button');
 const dialog = document.querySelector('dialog');
 const btn = document.querySelector('#submit');
+const form = document.querySelector('form');
 
 let bookName = document.querySelector('#name');
 let authorName = document.querySelector('#author');
 let bookPages = document.querySelector('#pages');
-let haveRead = document.querySelector('#haveRead');
+let haveRead = document.querySelector('#haveRead'); 
 
 modal.addEventListener('click', () => {
     dialog.showModal();
@@ -30,9 +45,11 @@ modal.addEventListener('click', () => {
 btn.addEventListener('click', (e) => {
     e.preventDefault();
     addBookToLibrary();
+    form.reset();
     dialog.close(); // close modal
     displayData();
 })
+
 
 function displayData() {
     // declared empty array to store all the values in each objects in {library} array
@@ -42,16 +59,43 @@ function displayData() {
         for(let j in library[i]) {
             data.push(library[i][j]);
         }
-        // for every index in library, create a row
-       let tr = document.createElement('tr');
-    
-        for(let k=0; k < data.length; k++) {
-            const td = document.createElement('td');
+    }
+    // for every index in library, create a row
+    let tr = document.createElement('tr');
+
+    for(let k=0; k < data.length; k++) {
+        const td = document.createElement('td');
+        if(data[k] === true || data[k] === false) { // to add a checkbox in td instead of text
+            td.appendChild(createCheckbox());
+            tr.appendChild(td);
+        } else {
             td.textContent = data[k];
             tr.appendChild(td);
         }
-        // finally append the row in table and wipe all data 
-        tdata.appendChild(tr);
-        data = [];
+
     }
+    if(data.length !== 0) { // add a delete button to the last column of a row
+        const td = document.createElement('td');
+        td.appendChild(createButton());
+        tr.appendChild(td);
+    }
+    // finally append the row in table and wipe all data 
+    tdata.appendChild(tr);
+    data = [];
+    
 }
+
+
+function deleteFromLibrary(index) {
+    index = index - 1;
+    library.splice(index, 1);    
+}
+
+tdata.addEventListener('click', (e) => {
+    if(e.target.matches('.delete-btn')) {
+
+        deleteFromLibrary(e.target.closest('tr').rowIndex);
+        e.target.closest('tr').remove();
+    } 
+  
+})
